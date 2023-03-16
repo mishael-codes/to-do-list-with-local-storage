e57f225ee215620e4670ff8b86258f9162e48ade
Mishael <enyimishael22@gmail.com>
2023-03-16 14:37:44 +0100

    Added styling to the page
    Styled the page to look more attractive and presentable

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ function displayItems(){
   let items = "";
   for(let i = 0; i < itemsArray.length; i++){
     items += `  <div class="item">
-                <div class="input-controller">
-                  <textarea disabled>${itemsArray[i]}</textarea>
+                <div class="input-controller d-flex mb-3">
+                  <textarea class="rounded-3 ps-2 me-2 pt-1" disabled>${itemsArray[i]}</textarea>
                   <div class="edit-controller">
                     <i class="fa-solid fa-check deleteBtn"></i>
                     <i class="fa-solid fa-pen-to-square editBtn"></i>
                   </div>
                 </div>
                 <div class="update-controller">
-                  <button class="saveBtn">Save</button>
-                  <button class="cancelBtn">Cancel</button>
+                  <button class="saveBtn rounded-3">Save</button>
+                  <button class="cancelBtn rounded-3">Cancel</button>
                 </div>
               </div>`
   }
